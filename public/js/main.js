d048018ba174a57846167973c4ac4c6fb69984c1
Gulshan-Prasad <gulshanprasad3114@gmail.com>
2025-08-24 19:24:34 +0530

    done

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-window.alert("linked")
+const plrmove = document.getElementById("plrmove")
+
+const rock = document.getElementById("rockbtn")
+const paper = document.getElementById("paperbtn")
+const scissor = document.getElementById("scissorbtn")
+
+rock.onclick = function() {
+    plrmove.src = "../public/images/Rock.png"
+}
+
+paper.onclick = function() {
+    plrmove.src = "../public/images/Paper.png"
+}
+
+scissor.onclick = function() {
+    plrmove.src = "../public/images/Scissor.png"
+}
+
+
+
+function togglePopup() {
+    let popup = document.getElementById("popup")
+    popup.classList.toggle("active");
+}
